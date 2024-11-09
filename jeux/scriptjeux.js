b162243ea01a62043339bff9cae99181568436f0
MaximeEsteves <150292881+MaximeEsteves@users.noreply.github.com>
2024-11-09 10:35:46 +0100

    Update scriptjeux.js

--- jeux/scriptjeux.js
+++ jeux/scriptjeux.js
@@ -25,12 +25,9 @@ function initialiserBoutons() {
 function afficherIndice() {
     if (titreJeux.innerText === "Facile") {
         imageIndice.src = image[0];
-        console.log(image[0]);
-        
     }
     if (titreJeux.innerText  === "Moyen") {
         imageIndice.src = image[1];
-        console.log(image[1]);
     }
     if (titreJeux.innerText  === "Difficile") {
         imageIndice.src = image[2];
@@ -60,53 +57,73 @@ function commencer(){
     };
 }
 function changerFacile() {
+    jeuxFacile.style.color ="rgb(200, 226, 50)";
     jeuxFacile.innerText = box[compteurFacile];
     compteurFacile++;
     if (compteurFacile >= box.length) compteurFacile = 0;
 }
 
 function changerMoyen() {
+    jeuxMoyen.style.color ="rgb(26, 192, 192)";
     jeuxMoyen.innerText = box[compteurMoyen];
     compteurMoyen++;
     if (compteurMoyen >= box.length) compteurMoyen = 0;
 }
 
 function changerDifficile() {
+    jeuxDifficile.style.color ="rgb(130, 16, 158)";
     jeuxDifficile.innerText = box[compteurDifficile];
     compteurDifficile++;
     if (compteurDifficile >= box.length) compteurDifficile = 0;
 }
 
 function changerExtreme() {
+    jeuxExtreme.style.color ="rgb(190, 109, 33)";
     jeuxExtreme.innerText = box[compteurExtreme];
     compteurExtreme++;
     if (compteurExtreme >= box.length) compteurExtreme = 0;
 }
 
 function validerJeu() {
+    
     if (titreJeux.innerText === "Facile") {
+        const changeColor = (element, condition) => {
+            element.style.color = condition ? "green" : "red";
+        };
+        changeColor(jeuxFacile, jeuxFacile.innerText === "7");
         if (jeuxFacile.innerText === "7") {
             resultatJeux.innerText = "Bravo ! niveau moyen ";
             bouttonChangerMoyen.style.display = "block";
             imageIndice.style.display = "none"
             bouttonIndice.style.display = "block"
-
             testMoyen();
         } else {
             resultatJeux.innerText = "C'est faux, essaye encore";
         }
     } else if (titreJeux.innerText === "Moyen") {
-        if (jeuxFacile.innerText === "6" && jeuxMoyen.innerText === "6") {
+        const changeColor = (element, condition) => {
+            element.style.color = condition ? "green" : "red";
+        };
+        changeColor(jeuxFacile, jeuxFacile.innerText === "6");
+        changeColor(jeuxMoyen, jeuxMoyen.innerText === "6");
+    
+        if (jeuxMoyen.innerText === "6" && jeuxFacile.innerText === "6") {
             resultatJeux.innerText = "Bravo ! niveau difficile";
             bouttonChangerDifficile.style.display = "block";
-            imageIndice.style.display = "none"
-            bouttonIndice.style.display = "block"
-
+            imageIndice.style.display = "none";
+            bouttonIndice.style.display = "block";
             testDifficile();
-        } else {
-            resultatJeux.innerText = "C'est faux, essaye encore";
         }
+    
+    
+    
     } else if (titreJeux.innerText === "Difficile") {
+        const changeColor = (element, condition) => {
+            element.style.color = condition ? "green" : "red";
+        };
+        changeColor(jeuxFacile, jeuxFacile.innerText === "1");
+        changeColor(jeuxMoyen, jeuxMoyen.innerText === "7");
+        changeColor(jeuxDifficile, jeuxDifficile.innerText === "8");
         if (jeuxFacile.innerText === "1" && jeuxMoyen.innerText === "7" && jeuxDifficile.innerText === "8") {
             resultatJeux.innerText = "Bravo ! niveau extreme";
             bouttonChangerExtreme.style.display = "block";
@@ -118,6 +135,13 @@ function validerJeu() {
             resultatJeux.innerText = "C'est faux, essaye encore";
         }
     } else if (titreJeux.innerText === "Extreme") {
+        const changeColor = (element, condition) => {
+            element.style.color = condition ? "green" : "red";
+        };
+        changeColor(jeuxFacile, jeuxFacile.innerText === "2");
+        changeColor(jeuxMoyen, jeuxMoyen.innerText === "5");
+        changeColor(jeuxDifficile, jeuxDifficile.innerText === "7");
+        changeColor(jeuxExtreme, jeuxExtreme.innerText === "7");
         if (jeuxFacile.innerText === "2" && jeuxMoyen.innerText === "5" && jeuxDifficile.innerText === "7" && jeuxExtreme.innerText === "7") {
             bouttonRejouer.style.display = "block"
             imageIndice.style.display = "none"
@@ -158,13 +182,17 @@ function rejouer() {
    compteurExtreme = 8
  
 
-    resultatJeux.innerText = "Clique sur le bouton 'Commencer' puis utilise le bouton 'Click' pour jouer"
+    resultatJeux.innerText = "Clique sur le bouton 'Commencer' puis utilise le bouton 'Clique' pour jouer"
     titreJeux.innerText = "Facile";
     jeuxFacile.innerText = compteurFacile;
     jeuxMoyen.innerText = compteurMoyen;
     jeuxDifficile.innerText = compteurDifficile;
     jeuxExtreme.innerText = compteurExtreme;
     
+    jeuxFacile.style.color = "#aec4b8";
+    jeuxMoyen.style.color ="#aec4b8";
+    jeuxDifficile.style.color ="#aec4b8";
+    jeuxExtreme.style.color ="#aec4b8";
     bouttonRejouer.style.display = "none";
     bouttonIndice.style.display = "none";
     imageIndice.style.display = "none";
