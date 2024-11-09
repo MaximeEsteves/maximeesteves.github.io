d367875783ef29505a7b635e562301295ec010a8
MaximeEsteves <150292881+MaximeEsteves@users.noreply.github.com>
2024-11-09 10:35:35 +0100

    Update configjeux.js

--- jeux/configjeux.js
+++ jeux/configjeux.js
@@ -2,16 +2,16 @@ const box =[1 ,2, 3, 4, 5, 6, 7, 8, 9]
 const image=[
     "jeux/indice/indicefacile.png",
     "jeux/indice/indicemoyen.png",
-    "jeux/indice/indicedifficile.jpg", 
+    "jeux/indice/indicedifficile.png", 
     "jeux/indice/indiceextreme.png"]
 
 const titreJeux = document.querySelector("h2")
 const resultatJeux = document.querySelector("h3")
 const grille = document.querySelectorAll("p")
-const jeuxFacile = document.getElementById("5")
-const jeuxMoyen = document.getElementById("1")
-const jeuxDifficile = document.getElementById("3")
-const jeuxExtreme = document.getElementById("8")
+const jeuxFacile = document.getElementById("a5")
+const jeuxMoyen = document.getElementById("a1")
+const jeuxDifficile = document.getElementById("a3")
+const jeuxExtreme = document.getElementById("a8")
 const bouttonCommencer = document.getElementById("commencer")
 const bouttonChanger = document.getElementById("changerFacile")
 const bouttonValider = document.getElementById("valider")
@@ -31,3 +31,15 @@ let compteurExtreme = 8
 let secondes = 0
 let timerInterval
 let click = 0
+
+
+//Ajouter le chiffreTrouver/chiffreTotal à chaque fois que le joueur clique sur valider quand il y à une erreur.
+//Lier une couleur sur le bouton à la case.
+//
+//
+//
+//
+//
+//
+//
+//
